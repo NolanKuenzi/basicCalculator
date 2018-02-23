102b762f9b3283cd50c77d8d63e1f1b544a76ab9
Nolan Kuenzi <mr.nolank@gmail.com>
2018-02-22 21:57:41 -0800

    v0 of html, css and js

---
+++ fcc_calc.js
@@ -0,0 +1,26 @@
+
+let del = () => {
+	calc.answer.value = calc.answer.value.replace(calc.answer.value[calc.answer.value.length-1], "");
+}
+
+let plus_mine = () => {
+	if (Math.sign(calc.answer.value[0]) === 1 || calc.answer.value === "") {
+		calc.answer.value = "-" + calc.answer.value;
+   } else {
+		calc.answer.value = calc.answer.value.replace("-", "");
+	}
+}
+
+let solve = () => {
+	if (calc.answer.value === "") {
+		alert("Please Enter Values to Calculate");
+	} else {
+		try { 
+			calc.answer.value = eval(calc.answer.value);
+		}
+		catch(error) {
+			calc.answer.value = "Syntax Error";
+		}
+	} 
+
+}
