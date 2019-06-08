872086e8095d08cbf07eb4078f778834140a11ca
Nolan Kuenzi <mr.nolank@gmail.com>
2019-06-08 12:26:35 -0700

    babel update

--- public/index.js
+++ public/index.js
@@ -1,25 +1,25 @@
 "use strict";
 
 var del = function del() {
-    calc.answer.value = calc.answer.value.replace(calc.answer.value[calc.answer.value.length - 1], "");
+  calc.answer.value = calc.answer.value.replace(calc.answer.value[calc.answer.value.length - 1], "");
 };
 
 var plus_mine = function plus_mine() {
-    if (Math.sign(calc.answer.value[0]) === 1 || calc.answer.value === "") {
-        calc.answer.value = "-" + calc.answer.value;
-    } else {
-        calc.answer.value = calc.answer.value.replace("-", "");
-    }
+  if (Math.sign(calc.answer.value[0]) === 1 || calc.answer.value === "") {
+    calc.answer.value = "-" + calc.answer.value;
+  } else {
+    calc.answer.value = calc.answer.value.replace("-", "");
+  }
 };
 
 var solve = function solve() {
-    if (calc.answer.value === "") {
-        alert("Please Enter Values to Calculate");
-    } else {
-        try {
-            calc.answer.value = eval(calc.answer.value);
-        } catch (error) {
-            calc.answer.value = "Syntax Error";
-        }
+  if (calc.answer.value === "") {
+    alert("Please Enter Values to Calculate");
+  } else {
+    try {
+      calc.answer.value = eval(calc.answer.value);
+    } catch (error) {
+      calc.answer.value = "Syntax Error";
     }
+  }
 };
